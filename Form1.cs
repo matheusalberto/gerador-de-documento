745ebde4c7661669b85d7289b3a84148884c0568
Matheus Alberto <maatheusaalberto@gmail.com>
2018-04-21 15:05:26 -0300

    Inserção das listas no preenchimento de taxas para quebrar linhas quando encontrar \n

--- Form1.cs
+++ Form1.cs
@@ -47,7 +47,17 @@ namespace Gerador
             myObject.valor_total = txtTotal.Text;
             myObject.adiantado = txtAdiantado.Text;
             myObject.devedor = txtDevedor.Text;
-            myObject.taxas = txtTaxas.Text;
+
+            List<dynamic> taxas = new List<dynamic>();
+            var AllTaxas = txtTaxas.Text.Split('\n');
+            foreach(var t in AllTaxas)
+            {
+                dynamic obj = new ExpandoObject();
+                obj.taxa = t;
+                taxas.Add(obj);
+            }
+
+            myObject.taxas = taxas;
 
             var dir = Directory.GetCurrentDirectory();
             var FilePath = dir + "/output.json";
@@ -56,7 +66,6 @@ namespace Gerador
                 File.Delete(FilePath);
 
             var jsonObj = JsonConvert.SerializeObject(myObject);
-
             using (System.IO.StreamWriter sw = new System.IO.StreamWriter(FilePath, true))
             {
                 sw.WriteLine(jsonObj);
@@ -65,7 +74,7 @@ namespace Gerador
 
             System.Diagnostics.Process.Start(string.Format("{0}/generador.bat", dir));
 
-            System.Threading.Thread.Sleep(5000);
+            System.Threading.Thread.Sleep(4000);
             var FileDocxName = dir + "/filename.txt";
             var ContentDocxFile = File.ReadAllText(FileDocxName);
             File.Delete(FileDocxName);            
